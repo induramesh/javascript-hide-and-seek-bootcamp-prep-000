1ef559acf6030a06987114abdecd772b5df13534
Indu Ramesh <induramesh@gmail.com>
2018-07-25 14:17:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ function nestedTarget() {
   return document.querySelector("#nested .target");
 }
 
-const lis = document.querySelectorAll('ul.ranked-list li')
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (i + n).toString()
+}
+}
