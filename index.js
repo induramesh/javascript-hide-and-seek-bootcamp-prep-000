f188619d43a898cfd43267606bfe7ab2e98a99ba
Indu Ramesh <induramesh@gmail.com>
2018-07-25 14:45:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector("" + selector);
+}
 
+function nestedTarget() {
+  return document.querySelector("#nested .target");
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list')
+for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(i + n).toString()
+ }
+}
+
+function deepestChild() {
+var nodes = document.querySelectorAll('#grand-node div');
+return nodes[nodes.length- 1];
+}
