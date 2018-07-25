10d623e0c18ba17428e843ab10ae07590112bd48
Indu Ramesh <induramesh@gmail.com>
2018-07-25 14:18:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.querySelectorAll('ul.ranked-list li')
+  const lis = document.querySelectorAll('ul.ranked-list')
 for (let i = 0; i < lis.length; i++) {
   lis[i].innerHTML = (i + n).toString()
 }
